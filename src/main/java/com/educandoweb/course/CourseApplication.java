d1e347c4cb54ee273dddb8b3daabaab1ad2e66fb
Murillo Nonato <murillo.murillo2001@gmail.com>
2022-01-23 22:06:52 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,8 +2,10 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.educandoweb.course.resources"})
 public class CourseApplication {
 
 	public static void main(String[] args) {
